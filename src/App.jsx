f9c40d716ef598128abc83958442765421a22d19
Arjay Altamera <altameraarjay7@gmail.com>
2025-05-12 09:19:05 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import HeroSection from './components/HeroSection';
+import FeaturedSection from './components/FeaturedSection';
+import BlogSection from './components/BlogSection';
+import React, { useEffect, useState } from "react";
+import Loader from "./components/Loader";  
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2500); // simulate loading
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading ? <Loader /> : 
+    <>
+      <HeroSection /> 
+      <FeaturedSection />
+      <BlogSection />
+    </>;
+}
+
+export default App
---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,61 @@
+
+
+export default () => {
+
+    const features = [
+        {
+            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+          </svg>,
+            title: "Front-End Development",
+            desc: "I offer expert development services in HTML, CSS, and JavaScript to build websites that are both functional and stylish. From layout design to custom interactivity, I ensure your website performs seamlessly and looks great."
+        },
+        {
+            icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" /></svg>,
+            title: "Responsive Web Design",
+            desc: "I specialize in making websites mobile-friendly. Your website will automatically adapt to any screen size, providing users with the best experience across all devices, including smartphones, tablets, and desktops."
+        },
+        {
+            icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" /></svg>,
+            title: "UI/UX Design & Optimization",
+            desc: "I focus on creating intuitive user interfaces and optimized user experiences, making sure your visitors not only enjoy using your site but also convert into customers."
+        },
+    ]
+
+    return (
+        <section className="py-20">
+            <div className="max-w-screen-xl mx-auto px-4 text-center text-gray-600 md:px-8">
+                <div className="max-w-2xl mx-auto">
+                    <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                        My Services
+                    </h3>
+                    <p className="mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus.
+                    </p>
+                </div>
+                <div className="mt-12">
+                    <ul className="grid gap-y-8 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+                        {
+                            features.map((item, idx) => (
+                                <li key={idx} className="space-y-3">
+                                    <div className="w-12 h-12 mx-auto bg-indigo-50 text-[#c163c1] rounded-full flex items-center justify-center">
+                                        {item.icon}
+                                    </div>
+                                    <h4 className="text-lg text-gray-800 font-semibold">
+                                        {item.title}
+                                    </h4>
+                                    <p>
+                                        {item.desc}
+                                    </p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
